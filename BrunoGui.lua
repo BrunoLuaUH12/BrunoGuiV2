eab69e75e6521561d6631f955661eab9f5d566f4
BrunoLuaUH12 <92273855+BrunoLuaUH12@users.noreply.github.com>
2021-10-10 23:19:17 -0300

    Update BrunoGui.lua

--- BrunoGui.lua
+++ BrunoGui.lua
@@ -26,7 +26,7 @@ local TextLabel = Instance.new("TextLabel")
 local UICorner_9 = Instance.new("UICorner")
 local abrirFrame = Instance.new("Frame")
 local UICorner_10 = Instance.new("UICorner")
-local TextButton = Instance.new("TextButton")
+local abrir = Instance.new("TextButton")
 local UICorner_11 = Instance.new("UICorner")
 local gamesframe = Instance.new("Frame")
 local ScrollingFrame_2 = Instance.new("ScrollingFrame")
@@ -162,7 +162,7 @@ TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
 TextLabel.Position = UDim2.new(0.131535247, 0, 0.0231578946, 0)
 TextLabel.Size = UDim2.new(0, 251, 0, 50)
 TextLabel.Font = Enum.Font.SciFi
-TextLabel.Text = "BrunoGui V2.2"
+TextLabel.Text = "BrunoGui V2.1"
 TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.TextScaled = true
 TextLabel.TextSize = 14.000
@@ -180,20 +180,21 @@ abrirFrame.Size = UDim2.new(0, 114, 0, 62)
 
 UICorner_10.Parent = abrirFrame
 
-TextButton.Parent = abrirFrame
-TextButton.BackgroundColor3 = Color3.fromRGB(153, 153, 153)
-TextButton.BackgroundTransparency = 0.350
-TextButton.BorderColor3 = Color3.fromRGB(153, 153, 153)
-TextButton.Position = UDim2.new(0.113931224, 0, 0.176383719, 0)
-TextButton.Size = UDim2.new(0, 89, 0, 43)
-TextButton.Font = Enum.Font.Michroma
-TextButton.Text = "Abrir"
-TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextButton.TextScaled = true
-TextButton.TextSize = 14.000
-TextButton.TextWrapped = true
+abrir.Name = "abrir"
+abrir.Parent = abrirFrame
+abrir.BackgroundColor3 = Color3.fromRGB(153, 153, 153)
+abrir.BackgroundTransparency = 0.350
+abrir.BorderColor3 = Color3.fromRGB(153, 153, 153)
+abrir.Position = UDim2.new(0.113931224, 0, 0.176383719, 0)
+abrir.Size = UDim2.new(0, 89, 0, 43)
+abrir.Font = Enum.Font.Michroma
+abrir.Text = "Abrir"
+abrir.TextColor3 = Color3.fromRGB(0, 0, 0)
+abrir.TextScaled = true
+abrir.TextSize = 14.000
+abrir.TextWrapped = true
 
-UICorner_11.Parent = TextButton
+UICorner_11.Parent = abrir
 
 gamesframe.Name = "gamesframe"
 gamesframe.Parent = ScreenGui
@@ -254,7 +255,72 @@ UICorner_14.Parent = gamesframe
 
 -- Scripts:
 
-local function CWTW_fake_script() -- Frame.LocalScript 
+local function CSNC_fake_script() -- owl.LocalScript 
+	local script = Instance.new('LocalScript', owl)
+
+	script.Parent.Frame.ScrollingFrame.owl.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))();
+	end)
+end
+coroutine.wrap(CSNC_fake_script)()
+local function SUXKQAH_fake_script() -- spy.LocalScript 
+	local script = Instance.new('LocalScript', spy)
+
+	script.Parent.Frame.ScrollingFrame.spy.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet("https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua"))()
+	end)
+end
+coroutine.wrap(SUXKQAH_fake_script)()
+local function OHEMQ_fake_script() -- ez.LocalScript 
+	local script = Instance.new('LocalScript', ez)
+
+	script.Parent.Frame.ScrollingFrame.ez.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet(('https://raw.githubusercontent.com/debug420/Ez-Industries-Launcher-Data/master/Launcher.lua'),true))()
+	end)
+end
+coroutine.wrap(OHEMQ_fake_script)()
+local function FZABTEO_fake_script() -- iy.LocalScript 
+	local script = Instance.new('LocalScript', iy)
+
+	script.Parent.Frame.ScrollingFrame.iy.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+	end)
+end
+coroutine.wrap(FZABTEO_fake_script)()
+local function MFVMF_fake_script() -- cmd.LocalScript 
+	local script = Instance.new('LocalScript', cmd)
+
+	script.Parent.Frame.ScrollingFrame.cmd.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
+	end)
+end
+coroutine.wrap(MFVMF_fake_script)()
+local function FHPY_fake_script() -- games.LocalScript 
+	local script = Instance.new('LocalScript', games)
+
+	script.Parent.Frame.ScrollingFrame.games.MouseButton1Click:Connect(function()
+		script.Parent.gamesframe.Visible = true
+	end)
+end
+coroutine.wrap(FHPY_fake_script)()
+local function YWWIGUW_fake_script() -- close.LocalScript 
+	local script = Instance.new('LocalScript', close)
+
+	script.Parent.Frame.ScrollingFrame.close.MouseButton1Click:Connect(function()
+		script.Parent.abrirFrame.Visible = true
+		script.Parent.Frame.Visible = false
+	end)
+end
+coroutine.wrap(YWWIGUW_fake_script)()
+local function HHEWD_fake_script() -- dex.LocalScript 
+	local script = Instance.new('LocalScript', dex)
+
+	script.Parent.Frame.ScrollingFrame.dex.MouseButton1Click:Connect(function()
+		loadstring(game:GetObjects("rbxassetid://418957341")[1].Source)()
+	end)
+end
+coroutine.wrap(HHEWD_fake_script)()
+local function MCGQP_fake_script() -- Frame.LocalScript 
 	local script = Instance.new('LocalScript', Frame)
 
 	local UserInputService = game:GetService("UserInputService")
@@ -297,64 +363,8 @@ local function CWTW_fake_script() -- Frame.LocalScript
 		end
 	end)
 end
-coroutine.wrap(CWTW_fake_script)()
-local function RIJFZ_fake_script() -- ScreenGui.LocalScript 
-	local script = Instance.new('LocalScript', ScreenGui)
-
-	
-	script.Parent.abrirFrame.TextButton.MouseButton1Click:Connect(function()
-		script.Parent.abrirFrame.Visible = false
-		script.Parent.Frame.Visible = true
-	end)
-	
-	script.Parent.Frame.ScrollingFrame.close.MouseButton1Click:Connect(function()
-		script.Parent.abrirFrame.Visible = true
-		script.Parent.Frame.Visible = false
-	end)
-	
-	script.Parent.Frame.ScrollingFrame.games.MouseButton1Click:Connect(function()
-		script.Parent.gamesframe.Visible = true
-	end)
-	
-	script.Parent.gamesframe.ScrollingFrame.closegames.MouseButton1Click:Connect(function()
-		script.Parent.gamesframe.Visible = false
-	end)
-	
-	script.Parent.gamesframe.ScrollingFrame.anomic.MouseButton1Click:Connect(function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Genokoe/AnomicMain/main/loader"))()
-	end)
-	
-	script.Parent.Frame.ScrollingFrame.ez.MouseButton1Click:Connect(function()
-		loadstring(game:HttpGet(('https://raw.githubusercontent.com/debug420/Ez-Industries-Launcher-Data/master/Launcher.lua'),true))()
-	end)
-	
-	script.Parent.Frame.ScrollingFrame.dex.MouseButton1Click:Connect(function()
-		loadstring(game:GetObjects("rbxassetid://418957341")[1].Source)()
-	end)
-	
-	script.Parent.Frame.ScrollingFrame.spy.MouseButton1Click:Connect(function()
-		loadstring(game:HttpGet("https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua"))()
-	end)
-	
-	script.Parent.Frame.ScrollingFrame.cmd.MouseButton1Click:Connect(function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
-	end)
-	
-	script.Parent.Frame.ScrollingFrame.iy.MouseButton1Click:Connect(function()
-		loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-	end)
-	
-	script.Parent.Frame.ScrollingFrame.owl.MouseButton1Click:Connect(function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))();
-	end)
-	
-	
-	
-	
-	
-end
-coroutine.wrap(RIJFZ_fake_script)()
-local function OYOCSYS_fake_script() -- abrirFrame.LocalScript 
+coroutine.wrap(MCGQP_fake_script)()
+local function FAAXH_fake_script() -- abrirFrame.LocalScript 
 	local script = Instance.new('LocalScript', abrirFrame)
 
 	local UserInputService = game:GetService("UserInputService")
@@ -397,8 +407,33 @@ local function OYOCSYS_fake_script() -- abrirFrame.LocalScript
 		end
 	end)
 end
-coroutine.wrap(OYOCSYS_fake_script)()
-local function XAZDTWH_fake_script() -- gamesframe.LocalScript 
+coroutine.wrap(FAAXH_fake_script)()
+local function HVES_fake_script() -- abrir.LocalScript 
+	local script = Instance.new('LocalScript', abrir)
+
+	script.Parent.abrirFrame.abrir.MouseButton1Click:Connect(function()
+		script.Parent.abrirFrame.Visible = false
+		script.Parent.Frame.Visible = true
+	end)
+end
+coroutine.wrap(HVES_fake_script)()
+local function NMAX_fake_script() -- anomic.LocalScript 
+	local script = Instance.new('LocalScript', anomic)
+
+	script.Parent.gamesframe.ScrollingFrame.anomic.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Genokoe/AnomicMain/main/loader"))()
+	end)
+end
+coroutine.wrap(NMAX_fake_script)()
+local function GWGL_fake_script() -- closegames.LocalScript 
+	local script = Instance.new('LocalScript', closegames)
+
+	script.Parent.gamesframe.ScrollingFrame.closegames.MouseButton1Click:Connect(function()
+		script.Parent.gamesframe.Visible = false
+	end)
+end
+coroutine.wrap(GWGL_fake_script)()
+local function RCBZ_fake_script() -- gamesframe.LocalScript 
 	local script = Instance.new('LocalScript', gamesframe)
 
 	local UserInputService = game:GetService("UserInputService")
@@ -441,4 +476,4 @@ local function XAZDTWH_fake_script() -- gamesframe.LocalScript
 		end
 	end)
 end
-coroutine.wrap(XAZDTWH_fake_script)()
+coroutine.wrap(RCBZ_fake_script)()
