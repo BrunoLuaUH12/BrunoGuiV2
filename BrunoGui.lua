932cab3557ee4b54955f012db86f20d245c71d6e
BrunoLuaUH12 <92273855+BrunoLuaUH12@users.noreply.github.com>
2021-10-11 15:23:21 -0300

    Update BrunoGui.lua

--- BrunoGui.lua
+++ BrunoGui.lua
@@ -4,52 +4,91 @@
 -- Instances:
 
 local BrunoGuiV2 = Instance.new("ScreenGui")
+local abrirFrame = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local abrir = Instance.new("TextButton")
+local UICorner_2 = Instance.new("UICorner")
+local Fundo = Instance.new("Frame")
+local UICorner_3 = Instance.new("UICorner")
 local Frame = Instance.new("Frame")
 local ScrollingFrame = Instance.new("ScrollingFrame")
 local owl = Instance.new("TextButton")
-local UICorner = Instance.new("UICorner")
-local spy = Instance.new("TextButton")
-local UICorner_2 = Instance.new("UICorner")
-local ez = Instance.new("TextButton")
-local UICorner_3 = Instance.new("UICorner")
-local iy = Instance.new("TextButton")
 local UICorner_4 = Instance.new("UICorner")
-local cmd = Instance.new("TextButton")
+local spy = Instance.new("TextButton")
 local UICorner_5 = Instance.new("UICorner")
-local games = Instance.new("TextButton")
+local ez = Instance.new("TextButton")
 local UICorner_6 = Instance.new("UICorner")
-local close = Instance.new("TextButton")
+local iy = Instance.new("TextButton")
 local UICorner_7 = Instance.new("UICorner")
-local dex = Instance.new("TextButton")
+local cmd = Instance.new("TextButton")
 local UICorner_8 = Instance.new("UICorner")
-local TextLabel = Instance.new("TextLabel")
+local games = Instance.new("TextButton")
 local UICorner_9 = Instance.new("UICorner")
-local abrirFrame = Instance.new("Frame")
+local close = Instance.new("TextButton")
 local UICorner_10 = Instance.new("UICorner")
-local abrir = Instance.new("TextButton")
+local dex = Instance.new("TextButton")
 local UICorner_11 = Instance.new("UICorner")
+local TextLabel = Instance.new("TextLabel")
+local UICorner_12 = Instance.new("UICorner")
+local FundoGames = Instance.new("Frame")
 local gamesframe = Instance.new("Frame")
 local ScrollingFrame_2 = Instance.new("ScrollingFrame")
 local closegames = Instance.new("TextButton")
-local UICorner_12 = Instance.new("UICorner")
-local speedsimulator = Instance.new("TextButton")
 local UICorner_13 = Instance.new("UICorner")
-local HEROSinifinity = Instance.new("TextButton")
+local speedsimulator = Instance.new("TextButton")
 local UICorner_14 = Instance.new("UICorner")
-local TextLabel_2 = Instance.new("TextLabel")
+local HEROSinifinity = Instance.new("TextButton")
 local UICorner_15 = Instance.new("UICorner")
+local TextLabel_2 = Instance.new("TextLabel")
+local UICorner_16 = Instance.new("UICorner")
+local UICorner_17 = Instance.new("UICorner")
 
 --Properties:
 
 BrunoGuiV2.Name = "BrunoGuiV2"
 BrunoGuiV2.Parent = game.CoreGui
 
-Frame.Parent = BrunoGuiV2
+abrirFrame.Name = "abrirFrame"
+abrirFrame.Parent = BrunoGuiV2
+abrirFrame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+abrirFrame.BackgroundTransparency = 0.400
+abrirFrame.Position = UDim2.new(0.102189071, 0, 0.741104305, 0)
+abrirFrame.Size = UDim2.new(0, 114, 0, 62)
+
+UICorner.Parent = abrirFrame
+
+abrir.Name = "abrir"
+abrir.Parent = abrirFrame
+abrir.BackgroundColor3 = Color3.fromRGB(153, 153, 153)
+abrir.BackgroundTransparency = 0.350
+abrir.BorderColor3 = Color3.fromRGB(153, 153, 153)
+abrir.Position = UDim2.new(0.113931224, 0, 0.176383719, 0)
+abrir.Size = UDim2.new(0, 89, 0, 43)
+abrir.Font = Enum.Font.Michroma
+abrir.Text = "Abrir"
+abrir.TextColor3 = Color3.fromRGB(0, 0, 0)
+abrir.TextScaled = true
+abrir.TextSize = 14.000
+abrir.TextWrapped = true
+
+UICorner_2.Parent = abrir
+
+Fundo.Name = "Fundo"
+Fundo.Parent = BrunoGuiV2
+Fundo.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Fundo.BackgroundTransparency = 0.700
+Fundo.Position = UDim2.new(0.223446116, 0, 0.265030682, 0)
+Fundo.Size = UDim2.new(0, 354, 0, 493)
+Fundo.Visible = false
+
+UICorner_3.CornerRadius = UDim.new(0, 50)
+UICorner_3.Parent = Fundo
+
+Frame.Parent = Fundo
 Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 Frame.BackgroundTransparency = 0.400
-Frame.Position = UDim2.new(0.236902043, 0, 0.290797532, 0)
+Frame.Position = UDim2.new(0.0476365089, 0, 0.041304633, 0)
 Frame.Size = UDim2.new(0, 320, 0, 452)
-Frame.Visible = false
 
 ScrollingFrame.Parent = Frame
 ScrollingFrame.Active = true
@@ -69,7 +108,7 @@ owl.Text = "OwlHub"
 owl.TextColor3 = Color3.fromRGB(0, 0, 0)
 owl.TextSize = 14.000
 
-UICorner.Parent = owl
+UICorner_4.Parent = owl
 
 spy.Name = "spy"
 spy.Parent = ScrollingFrame
@@ -81,7 +120,7 @@ spy.Text = "Simple Spy"
 spy.TextColor3 = Color3.fromRGB(0, 0, 0)
 spy.TextSize = 14.000
 
-UICorner_2.Parent = spy
+UICorner_5.Parent = spy
 
 ez.Name = "ez"
 ez.Parent = ScrollingFrame
@@ -93,7 +132,7 @@ ez.Text = "Ez Hub"
 ez.TextColor3 = Color3.fromRGB(0, 0, 0)
 ez.TextSize = 14.000
 
-UICorner_3.Parent = ez
+UICorner_6.Parent = ez
 
 iy.Name = "iy"
 iy.Parent = ScrollingFrame
@@ -105,7 +144,7 @@ iy.Text = "Infinity Yeld"
 iy.TextColor3 = Color3.fromRGB(0, 0, 0)
 iy.TextSize = 14.000
 
-UICorner_4.Parent = iy
+UICorner_7.Parent = iy
 
 cmd.Name = "cmd"
 cmd.Parent = ScrollingFrame
@@ -117,7 +156,7 @@ cmd.Text = "Cmd-X"
 cmd.TextColor3 = Color3.fromRGB(0, 0, 0)
 cmd.TextSize = 14.000
 
-UICorner_5.Parent = cmd
+UICorner_8.Parent = cmd
 
 games.Name = "games"
 games.Parent = ScrollingFrame
@@ -129,7 +168,7 @@ games.Text = "Games"
 games.TextColor3 = Color3.fromRGB(0, 0, 0)
 games.TextSize = 14.000
 
-UICorner_6.Parent = games
+UICorner_9.Parent = games
 
 close.Name = "close"
 close.Parent = ScrollingFrame
@@ -143,8 +182,8 @@ close.TextScaled = true
 close.TextSize = 14.000
 close.TextWrapped = true
 
-UICorner_7.CornerRadius = UDim.new(0, 10)
-UICorner_7.Parent = close
+UICorner_10.CornerRadius = UDim.new(0, 10)
+UICorner_10.Parent = close
 
 dex.Name = "dex"
 dex.Parent = ScrollingFrame
@@ -156,7 +195,7 @@ dex.Text = "Dex V.4"
 dex.TextColor3 = Color3.fromRGB(0, 0, 0)
 dex.TextSize = 14.000
 
-UICorner_8.Parent = dex
+UICorner_11.Parent = dex
 
 TextLabel.Parent = Frame
 TextLabel.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
@@ -171,41 +210,23 @@ TextLabel.TextScaled = true
 TextLabel.TextSize = 14.000
 TextLabel.TextWrapped = true
 
-UICorner_9.CornerRadius = UDim.new(0, 20)
-UICorner_9.Parent = Frame
+UICorner_12.CornerRadius = UDim.new(0, 20)
+UICorner_12.Parent = Frame
 
-abrirFrame.Name = "abrirFrame"
-abrirFrame.Parent = BrunoGuiV2
-abrirFrame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-abrirFrame.BackgroundTransparency = 0.400
-abrirFrame.Position = UDim2.new(0.102189071, 0, 0.741104305, 0)
-abrirFrame.Size = UDim2.new(0, 114, 0, 62)
-
-UICorner_10.Parent = abrirFrame
-
-abrir.Name = "abrir"
-abrir.Parent = abrirFrame
-abrir.BackgroundColor3 = Color3.fromRGB(153, 153, 153)
-abrir.BackgroundTransparency = 0.350
-abrir.BorderColor3 = Color3.fromRGB(153, 153, 153)
-abrir.Position = UDim2.new(0.113931224, 0, 0.176383719, 0)
-abrir.Size = UDim2.new(0, 89, 0, 43)
-abrir.Font = Enum.Font.Michroma
-abrir.Text = "Abrir"
-abrir.TextColor3 = Color3.fromRGB(0, 0, 0)
-abrir.TextScaled = true
-abrir.TextSize = 14.000
-abrir.TextWrapped = true
-
-UICorner_11.Parent = abrir
+FundoGames.Name = "FundoGames"
+FundoGames.Parent = BrunoGuiV2
+FundoGames.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+FundoGames.BackgroundTransparency = 0.700
+FundoGames.Position = UDim2.new(0.557828486, 0, 0.271165639, 0)
+FundoGames.Size = UDim2.new(0, 314, 0, 479)
+FundoGames.Visible = false
 
 gamesframe.Name = "gamesframe"
-gamesframe.Parent = BrunoGuiV2
+gamesframe.Parent = FundoGames
 gamesframe.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 gamesframe.BackgroundTransparency = 0.400
-gamesframe.Position = UDim2.new(0.578587711, 0, 0.290797532, 0)
+gamesframe.Position = UDim2.new(0.065848887, 0, 0.0444509834, 0)
 gamesframe.Size = UDim2.new(0, 272, 0, 436)
-gamesframe.Visible = false
 
 ScrollingFrame_2.Parent = gamesframe
 ScrollingFrame_2.Active = true
@@ -227,8 +248,8 @@ closegames.TextScaled = true
 closegames.TextSize = 14.000
 closegames.TextWrapped = true
 
-UICorner_12.CornerRadius = UDim.new(0, 10)
-UICorner_12.Parent = closegames
+UICorner_13.CornerRadius = UDim.new(0, 10)
+UICorner_13.Parent = closegames
 
 speedsimulator.Name = "speedsimulator"
 speedsimulator.Parent = ScrollingFrame_2
@@ -236,13 +257,13 @@ speedsimulator.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 speedsimulator.Position = UDim2.new(0.0250289142, 0, 0.00691692531, 0)
 speedsimulator.Size = UDim2.new(0, 93, 0, 46)
 speedsimulator.Font = Enum.Font.SciFi
-speedsimulator.Text = "Speed Simulator ⚡"
+speedsimulator.Text = "Speed Simulator"
 speedsimulator.TextColor3 = Color3.fromRGB(0, 0, 0)
 speedsimulator.TextScaled = true
 speedsimulator.TextSize = 14.000
 speedsimulator.TextWrapped = true
 
-UICorner_13.Parent = speedsimulator
+UICorner_14.Parent = speedsimulator
 
 HEROSinifinity.Name = "HEROSinifinity"
 HEROSinifinity.Parent = ScrollingFrame_2
@@ -256,7 +277,7 @@ HEROSinifinity.TextScaled = true
 HEROSinifinity.TextSize = 14.000
 HEROSinifinity.TextWrapped = true
 
-UICorner_14.Parent = HEROSinifinity
+UICorner_15.Parent = HEROSinifinity
 
 TextLabel_2.Parent = gamesframe
 TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -269,56 +290,15 @@ TextLabel_2.TextScaled = true
 TextLabel_2.TextSize = 14.000
 TextLabel_2.TextWrapped = true
 
-UICorner_15.CornerRadius = UDim.new(0, 20)
-UICorner_15.Parent = gamesframe
+UICorner_16.CornerRadius = UDim.new(0, 20)
+UICorner_16.Parent = gamesframe
+
+UICorner_17.CornerRadius = UDim.new(0, 50)
+UICorner_17.Parent = FundoGames
 
 -- Scripts:
 
-local function ILJHS_fake_script() -- Frame.LocalScript 
-	local script = Instance.new('LocalScript', Frame)
-
-	local UserInputService = game:GetService("UserInputService")
-	
-	local gui = script.Parent
-	
-	local dragging
-	local dragInput
-	local dragStart
-	local startPos
-	
-	local function update(input)
-		local delta = input.Position - dragStart
-		gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
-	end
-	
-	gui.InputBegan:Connect(function(input)
-		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-			dragging = true
-			dragStart = input.Position
-			startPos = gui.Position
-	
-			input.Changed:Connect(function()
-				if input.UserInputState == Enum.UserInputState.End then
-					dragging = false
-				end
-			end)
-		end
-	end)
-	
-	gui.InputChanged:Connect(function(input)
-		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-			dragInput = input
-		end
-	end)
-	
-	UserInputService.InputChanged:Connect(function(input)
-		if input == dragInput and dragging then
-			update(input)
-		end
-	end)
-end
-coroutine.wrap(ILJHS_fake_script)()
-local function AZEWRJU_fake_script() -- abrirFrame.LocalScript 
+local function VWCJKLA_fake_script() -- abrirFrame.LocalScript 
 	local script = Instance.new('LocalScript', abrirFrame)
 
 	local UserInputService = game:GetService("UserInputService")
@@ -361,9 +341,71 @@ local function AZEWRJU_fake_script() -- abrirFrame.LocalScript
 		end
 	end)
 end
-coroutine.wrap(AZEWRJU_fake_script)()
-local function ITYGCDJ_fake_script() -- gamesframe.LocalScript 
-	local script = Instance.new('LocalScript', gamesframe)
+coroutine.wrap(VWCJKLA_fake_script)()
+local function VKLM_fake_script() -- BrunoGuiV2.LocalScript 
+	local script = Instance.new('LocalScript', BrunoGuiV2)
+
+	-- Open and Close Principal
+	
+	script.Parent.abrirFrame.abrir.MouseButton1Click:Connect(function()
+		script.Parent.abrirFrame.Visible = false
+		script.Parent.Fundo.Visible = true
+	end)
+	
+	script.Parent.Fundo.Frame.ScrollingFrame.close.MouseButton1Click:Connect(function()
+		script.Parent.abrirFrame.Visible = true
+		script.Parent.Fundo.Visible = false
+	end)
+	
+	-- Open and Close Games Gui
+	
+	script.Parent.Fundo.Frame.ScrollingFrame.games.MouseButton1Click:Connect(function()
+		script.Parent.FundoGames.Visible = true
+	end)
+	
+	script.Parent.FundoGames.gamesframe.ScrollingFrame.closegames.MouseButton1Click:Connect(function()
+		script.Parent.FundoGames.Visible = false
+	end)
+	
+	-- Games Scripts
+	
+	script.Parent.FundoGames.gamesframe.ScrollingFrame.speedsimulator.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/BrunoLuaUH12/Scripts/main/Speed%20Simulator.lua'))()
+	end)
+	
+	script.Parent.FundoGames.gamesframe.ScrollingFrame.HEROSinifinity.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/BrunoLuaUH12/Scripts/main/HEROS%3A%20Infinity.lua'))()
+	end)
+	
+	-- Scripts
+	
+	script.Parent.Fundo.Frame.ScrollingFrame.cmd.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
+	end)
+	
+	script.Parent.Fundo.Frame.ScrollingFrame.dex.MouseButton1Click:Connect(function()
+		loadstring(game:GetObjects("rbxassetid://418957341")[1].Source)()
+	end)
+	
+	script.Parent.Fundo.Frame.ScrollingFrame.ez.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet(('https://raw.githubusercontent.com/debug420/Ez-Industries-Launcher-Data/master/Launcher.lua'),true))()
+	end)
+	
+	script.Parent.Fundo.Frame.ScrollingFrame.iy.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+	end)
+	
+	script.Parent.Fundo.Frame.ScrollingFrame.owl.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))();
+	end)
+	
+	script.Parent.Fundo.Frame.ScrollingFrame.spy.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet("https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua"))()
+	end)
+end
+coroutine.wrap(VKLM_fake_script)()
+local function VAENK_fake_script() -- Fundo.LocalScript 
+	local script = Instance.new('LocalScript', Fundo)
 
 	local UserInputService = game:GetService("UserInputService")
 	
@@ -405,66 +447,48 @@ local function ITYGCDJ_fake_script() -- gamesframe.LocalScript
 		end
 	end)
 end
-coroutine.wrap(ITYGCDJ_fake_script)()
-local function MKEWIM_fake_script() -- BrunoGuiV2.LocalScript 
-	local script = Instance.new('LocalScript', BrunoGuiV2)
+coroutine.wrap(VAENK_fake_script)()
+local function YBPW_fake_script() -- FundoGames.LocalScript 
+	local script = Instance.new('LocalScript', FundoGames)
 
-	-- Open and Close Principal
+	local UserInputService = game:GetService("UserInputService")
 	
-	script.Parent.abrirFrame.abrir.MouseButton1Click:Connect(function()
-		script.Parent.abrirFrame.Visible = false
-		script.Parent.Frame.Visible = true
+	local gui = script.Parent
+	
+	local dragging
+	local dragInput
+	local dragStart
+	local startPos
+	
+	local function update(input)
+		local delta = input.Position - dragStart
+		gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+	end
+	
+	gui.InputBegan:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+			dragging = true
+			dragStart = input.Position
+			startPos = gui.Position
+	
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragging = false
+				end
+			end)
+		end
 	end)
 	
-	script.Parent.Frame.ScrollingFrame.close.MouseButton1Click:Connect(function()
-		script.Parent.abrirFrame.Visible = true
-		script.Parent.Frame.Visible = false
+	gui.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			dragInput = input
+		end
 	end)
 	
-	-- Open and Close Games Gui
-	
-	script.Parent.Frame.ScrollingFrame.games.MouseButton1Click:Connect(function()
-		script.Parent.gamesframe.Visible = true
-	end)
-	
-	script.Parent.gamesframe.ScrollingFrame.closegames.MouseButton1Click:Connect(function()
-		script.Parent.gamesframe.Visible = false
-	end)
-	
-	-- Games Scripts
-	
-	script.Parent.gamesframe.ScrollingFrame.speedsimulator.MouseButton1Click:Connect(function()
-		loadstring(game:HttpGet('https://raw.githubusercontent.com/BrunoLuaUH12/Scripts/main/Speed%20Simulator.lua'))()
-	end)
-	
-	script.Parent.gamesframe.ScrollingFrame.HEROSinifinity.MouseButton1Click:Connect(function()
-		loadstring(game:HttpGet('https://raw.githubusercontent.com/BrunoLuaUH12/Scripts/main/HEROS%3A%20Infinity.lua'))()
-	end)
-	
-	-- Scripts
-	
-	script.Parent.Frame.ScrollingFrame.cmd.MouseButton1Click:Connect(function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
-	end)
-	
-	script.Parent.Frame.ScrollingFrame.dex.MouseButton1Click:Connect(function()
-		loadstring(game:GetObjects("rbxassetid://418957341")[1].Source)()
-	end)
-	
-	script.Parent.Frame.ScrollingFrame.ez.MouseButton1Click:Connect(function()
-		loadstring(game:HttpGet(('https://raw.githubusercontent.com/debug420/Ez-Industries-Launcher-Data/master/Launcher.lua'),true))()
-	end)
-	
-	script.Parent.Frame.ScrollingFrame.iy.MouseButton1Click:Connect(function()
-		loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-	end)
-	
-	script.Parent.Frame.ScrollingFrame.owl.MouseButton1Click:Connect(function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))();
-	end)
-	
-	script.Parent.Frame.ScrollingFrame.spy.MouseButton1Click:Connect(function()
-		loadstring(game:HttpGet("https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua"))()
+	UserInputService.InputChanged:Connect(function(input)
+		if input == dragInput and dragging then
+			update(input)
+		end
 	end)
 end
-coroutine.wrap(MKEWIM_fake_script)()
+coroutine.wrap(YBPW_fake_script)()
